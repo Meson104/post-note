5b654b5c11ba0eb2d9dc8e5a929ba4437423fd47
Kartikeya Lalge <kartikeya.lalge1043@gmail.com>
2026-01-30 22:30:57 +0530

    setters and getters

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,14 +1,35 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { verify } from "hono/jwt";
+
 
 export const blogRouter = new Hono<{
     Bindings : {
         DATABASE_URL : string;
+        JWT_SECRET : string;
+    },
+    Variables : {
+        userId : string
     }
 }>();
 
+blogRouter.use('/*', async (c,next)=>{
+    const authHeader = c.req.header("Authorization" ) || "";
+    const user = await verify(authHeader, c.env.JWT_SECRET,'HS256') as {id : string};
+    if(user) {
+        c.set("userId",user.id);
+        next();
+    }else {
+        c.status(403);
+        return c.json({
+            message : "you are not logged in!"
+        });
+    }
+})
+
 blogRouter.post('/', async (c) => {
+    const userId = c.get("userId");
     const body = await c.req.json();
     const prisma = new PrismaClient({datasourceUrl : c.env.DATABASE_URL }).$extends(withAccelerate());
 
@@ -17,7 +38,7 @@ blogRouter.post('/', async (c) => {
             data: {
                 title : body.title,
                 content : body.content,
-                authorId : 1
+                authorId : Number(userId)
             }
         })
         return c.json({id : blog.id})
